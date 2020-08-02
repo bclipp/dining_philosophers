ad6d7e4c225611c1bf3d6face010f3ee8e6eca92
blipp <bclipp770@gmail.com>
2020-08-02 07:40:02 -0400

    starting to define skel

---
+++ src/dining_philosophers/main.go
@@ -0,0 +1,44 @@
+package dining_philosophers
+
+import (
+	"fmt"
+	"sync"
+)
+
+//each chopstick a mutex
+//each phil is a go ruitine
+//each is associated with 2 chop l/r
+
+type Chopstick struct {sync.Mutex}
+
+type Philosopher struct {
+	leftChopstick, rightChopstick *Chopstick
+}
+
+func (p Philosopher) eat() {
+	for {
+		p.leftChopstick.Lock()
+		p.rightChopstick.Lock()
+
+		fmt.Print("eating")
+
+		p.rightChopstick.Unlock()
+		p.leftChopstick.Unlock()
+
+	}
+}
+
+func main() {
+
+	chopsticks := make([]*Chopstick,5)
+	for i := 0; i < 5; i++ {
+		chopsticks[i] = new(Chopstick)
+ 	}
+ 	philosophers := make([]*Philosopher,5)
+	for i := 0; i < 5; i++ {
+		philosophers = &Philosopher{
+			leftChopstick: chopsticks[i],
+			rightChopstick: chopsticks[(i+1)%5]
+		}
+	}
+}
